723619bbdb13311fa04a80970a032832bd095af5
Skarlett <Skarlett@users.noreply.github.com>
2017-12-20 00:25:54 -0600

    Delete init_app.py

--- src/init_app.py
+++
@@ -1,9 +0,0 @@
-from Settings import *
-from os import mkdir, path
-
-def check_dirs():
-  for d in [etc_folder, bin_folder, backup_folder, providers_folder]:
-    if not path.isdir(d):
-      logging.info('made directory: '+str(d))
-      mkdir(d)
-
